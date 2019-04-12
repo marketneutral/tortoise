92d38819e8342a93bd959164f7db07170df30416
marketneutral <jonathan.r.larkin@gmail.com>
2019-04-12 18:19:18 +0000

    fig bug in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='A Python machine learning library for tabular data, based on fastai v0.70',
     author='Jonathan Larkin',
     author_email='jonathan.r.larkin@gmail.com',
-    packages=find_packages(exclude('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs'))
 )
