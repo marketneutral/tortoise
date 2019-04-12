9156599f6dc5859c26e0b18833d2eb56d480eac3
marketneutral <jonathan.r.larkin@gmail.com>
2019-04-12 18:16:06 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tortoise',
+    version='0.70',
+    description='A Python machine learning library for tabular data, based on fastai v0.70',
+    author='Jonathan Larkin',
+    author_email='jonathan.r.larkin@gmail.com',
+    packages=find_packages(exclude('tests', 'docs'))
+)
